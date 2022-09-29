fd33defba700777b5b381f32e4f3d2f47459e62d
Hend123 <henda4544@gmail.com>
2022-09-29 14:50:16 -0700

    implement FakeDataSource

---
+++ app/src/test/java/com/udacity/project4/data/FakeDataSource.kt
@@ -0,0 +1,41 @@
+package com.udacity.project4.data
+
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+//Use FakeDataSource that act as test double to the localDataSource
+class FakeDataSource(val reminders: MutableList<ReminderDTO>) : ReminderDataSource {
+    private var shouldReturnError = false
+    fun setShouldReturnError(value: Boolean){
+        shouldReturnError = value
+
+    }
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        if(shouldReturnError){
+         return Result.Error("Reminders not Found")
+        }
+        reminders.let { return Result.Success(ArrayList(it)) }
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        reminders.add(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        if(shouldReturnError){
+            return Result.Error("Reminders not Found")
+        }
+        val reminder = reminders.find {
+            it.id == id
+        }
+        return if (reminder != null){
+            Result.Success(reminder)
+        }else{
+             Result.Error("Reminders not Found")
+        }
+    }
+
+    override suspend fun deleteAllReminders() {
+        reminders.clear()
+    }
+}
---
+++ app/src/test/java/com/udacity/project4/savereminder/SaveReminderViewModelTest.kt
@@ -0,0 +1,15 @@
+package com.udacity.project4.savereminder
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.data.FakeDataSource
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.runner.RunWith
+//test SaveReminderViewModelTest with fake data
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class SaveReminderViewModelTest {
+
+
+}
